8b72d5cde699268eeb3c486ad7f2ffbdb715bcba
Jerome Lim <jeromeintilalim@gmail.com>
2023-05-16 21:40:20 +0800

    commit

---
+++ api/Models/BookingDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace sad_react_WebAPI.Models
+{
+    public class BookingDBContext : DbContext
+    {
+        public BookingDBContext(DbContextOptions<BookingDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<EventClient> EventClients { get; set; }
+    }
+}
